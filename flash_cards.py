9466a0a854497241b5fd938b4fcd9a75d768e9ef
jingleman12 <jingleman12@users.noreply.github.com>
2025-04-04 20:40:19 -0400

    Add rectangle for first wrong answer

--- flash_cards.py
+++ flash_cards.py
@@ -32,19 +32,19 @@ X = int(input(f"Enter a number to focus on (or 0 to practice all numbers)?\n"))
 print("Get ready!")
 for i in range(3):
   print(f"{3-i}")
-  time.sleep(1)
+  time.sleep(0.1)
 print("Go!")
 time.sleep(1)
 
 t = time.time()
-n_wrong = 0
+n_wrong_total = 0
 
 for i in range(n):
   x = random.randint(0, 10) if X == 0 else X
   y = random.randint(0, 10)
   z = type.operation(x, y)
   
-  a = -1
+  a, n_wrong = -1, 0
   while a != z:
     a = input(f"{x} {type.symbol()} {y} = ")
     try:
@@ -53,13 +53,35 @@ for i in range(n):
       print("You did not enter a valid number.")
       continue
     if a != z:
+      n_wrong_total += 1
       n_wrong += 1
-      print(f"{x} {type.symbol()} {y} = {z}!!! Type that to show you are learning:")
+      if isinstance(type, MultiplicationType) and n_wrong == 1:
+        def rc_str(rc):
+          s = f"{rc + 1}"
+          while len(s) < 3:
+            s = " " + s
+          return s
+        print("\n   ", end="")
+        for c in range(y):
+          print(rc_str(c), end="")
+        print()
+        for r in range(x):
+          print(rc_str(r), end="")
+          for c in range(y):
+            print("  *", end="")
+          print()
+        print("\nTry again, counting the stars above if that helps.\n")
+      else:
+        print(f"{x} {type.symbol()} {y} = {z}!!! Type that to show you are learning:")
 
 dt = time.time() - t
+pct = 100 * (1 - n_wrong_total / n)
+now = datetime.datetime.now().strftime("%m/%d %H:%M")
+
 print("You finished {:d} {} problems in {:.1f} seconds!".format(n, type.word(), dt))
 print("That's {:.1f} seconds per problem!!".format(dt/n))
-print(f"You got {n_wrong} wrong out of {n}.")
-print("That's an accuracy of {:.1f}%!".format(100 * (1 - n_wrong / n)))
-print("{} {:.1f} sec/prob {:.1f} % {} prob X: {}".format(datetime.datetime.now(), dt/n, 100 * (1 - n_wrong / n), n, X), file=open("summaries.txt", "a"))
+print(f"You got {n_wrong_total} wrong out of {n}.")
+print("That's an accuracy of {:.1f}%!".format(pct))
 
+with open("summaries.txt", "a") as f:
+  print("{}: {:.1f} / {} = {:.1f} sec/prob, at {:.1f}% (#: {})".format(now, dt, n, dt/n, pct, X), file=f)
